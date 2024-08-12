61628e78d81cd7fc5076569355a57f942362f831
Syberite <165388033+Syberite@users.noreply.github.com>
2024-08-12 19:57:02 +0000

    d

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('template/login.html')
+    return render_template('template/index.html')
 
 
 if __name__ == '__main__':
