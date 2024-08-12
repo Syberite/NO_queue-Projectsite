5657d6c79bc0dbbfe3379e126944bc11ad2bbb85
Syberite <165388033+Syberite@users.noreply.github.com>
2024-08-12 19:56:05 +0000

    modiefied

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "hellow chutiya"
+    return render_template('template/login.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
