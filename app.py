afc2c0c1d310ddddff664058aeeb73826beaa02b
Syberite <165388033+Syberite@users.noreply.github.com>
2024-08-13 15:27:10 +0000

    ewf

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ def index():
     return render_template('index.html')
 
 # Login route
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        # Handle login logic here (e.g., authenticate user)
-        username = request.form['username']
-        password = request.form['password']
-        # Add authentication logic
-        return redirect(url_for('index'))  # Redirect to index after login
     return render_template('login.html')
+def client_dashboard():
+    return render_template('client_dashboard.html')
+def admin():
+    return render_template('admin.html')
+def SPDB():
+    return render_template('service_provider_dashboard.html')
+def slotbooking():
+    return render_template('slot_booking.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
